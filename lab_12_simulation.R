68133b4b04499211d96554ca431816372a6e5bc7
Akash Patwal <akash@Akashs-MBP.wv.cc.cmu.edu>
2018-04-17 13:00:53 -0400

    finished run_simulation

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,5 +1,5 @@
 generate_data = function(n, p) {
-  m = matrix(vector (length = n * p), nrow = n, col = p)
+  m = matrix(vector (length = n * p), nrow = n, ncol = p)
   for (i in 1:n) {
     m[i, ] = rnorm(p, mean = 0, sd = 1)
   }
@@ -12,9 +12,30 @@ model_select = function(covariates, responses, cutoff) {
   init_model = lm(responses ~ covariates) 
   reduced = summary(init_model)$coefficients[, 4] <= cutoff
   reduced_model = lm(responses ~ covariates[, reduced])
-  if (nrow(reduced_cov) == 0) {
+  if (sum(reduced) == 0) {
     return(v)
   }
   return(summary(reduced_model)$coefficients[, 4])
 }
+
+run_simulation = function(n_trials, n, p, cutoff) {
+  p_values = c()
+  for (trial in 1:n_trials) {
+    result_i = generate_data(n, p)
+    p_values_i = model_select(result_i[[1]], result_i[[2]], cutoff)
+    p_values = c(p_values, p_values_i)
+  }
+  hist(p_values, xlab = "P Values")
+}
+
+ns = c(100, 1000,10000)
+ps = c(10, 20, 50)
+cutoff = .05
+for (n in ns) {
+  for (p in ps) {
+    run_simulation(n_trials = 10, n = n, p = p, cutoff = cutoff)
+  }
+}
+
+
 
